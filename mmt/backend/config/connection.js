c5bb12e127d9f4c85d25bb49825d40172ce76a56
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-10-09 23:24:00 +0530

    users_added

--- mmt/backend/config/connection.js
+++ mmt/backend/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const mongodb_url = process.env.MONGODB_URL;
 
 //  const db = 'mongodb+srv://Akashpatel:Akash@2211@cluster0.wujpf2o.mongodb.net/blogapp?retryWrites=true&w=majority'
 const connection = async() =>{
 
    return new Promise((resolve,reject)=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/makemytrip').then(()=>{
+    mongoose.connect(mongodb_url).then(()=>{
         console.log('connected')
         resolve()
     }).catch((err)=>{
--- mmt/backend/server/index.js
+++ mmt/backend/server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const connection = require('../config/connection');
 const flightRouter = require('../Router/flight');
+const authRouter = require("./Router/auth.routes");
+require("dotenv").config();
+
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/flight',flightRouter);
+app.use("/auth", authRouter);
 
 
 
-app.listen(8080,()=>{
+app.listen(process.env.PORT,()=>{
     connection();
     console.log("start server");
 })
