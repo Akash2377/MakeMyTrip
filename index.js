f031ea16b5d30b7409850fb99e7cf491ed9091f0
Akash Surve <surveakash01@gmail.com>
2022-10-10 15:59:00 +0530

    update done

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const connection = require("./config/connection");
 const flightRouter = require("./Router/flight");
 const cors = require("cors");
-
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/flight", flightRouter);
-app.listen(8080, () => {
+app.listen(port, () => {
   connection();
   console.log("start server");
 });
