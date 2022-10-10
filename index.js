43937e79d04350c1d297e03671c528028fabf960
Akash Surve <surveakash01@gmail.com>
2022-10-10 16:53:49 +0530

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/flight", flightRouter);
+app.get("/", "Hello");
 app.listen(port, () => {
   connection();
   console.log("start server");
