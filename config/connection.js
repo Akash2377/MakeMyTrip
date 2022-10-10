43ac6bfd09841eea327491721be35d138e122085
Akash Surve <surveakash01@gmail.com>
2022-10-10 15:21:28 +0530

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 //  const db = 'mongodb+srv://Akashpatel:Akash@2211@cluster0.wujpf2o.mongodb.net/blogapp?retryWrites=true&w=majority'
 const connection = async () => {
   return new Promise((resolve, reject) => {
     mongoose
-      .connect("mongodb://127.0.0.1:27017/makemytrip")
+      .connect(
+        `mongodb+srv://${username}:${password}@cluster0.wujpf2o.mongodb.net/blogapp?retryWrites=true&w=majority`
+      )
       .then(() => {
         console.log("connected");
         resolve();
