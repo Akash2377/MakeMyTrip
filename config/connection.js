4c5101445215c37cb3f55ec82d7636537f797d8b
Akash Surve <surveakash01@gmail.com>
2022-10-10 15:40:29 +0530

    cloud changed

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
-//  const db = 'mongodb+srv://Akashpatel:Akash@2211@cluster0.wujpf2o.mongodb.net/blogapp?retryWrites=true&w=majority'
+//  const db = 'mongodb+srv://AkashSurve:<password>@cluster0.ttsrfa6.mongodb.net/?retryWrites=true&w=majority'
 const connection = async () => {
   return new Promise((resolve, reject) => {
     mongoose
       .connect(
-        `mongodb+srv://${username}:${password}@cluster0.wujpf2o.mongodb.net/blogapp?retryWrites=true&w=majority`
+        `mongodb+srv://${username}:${password}@cluster0.ttsrfa6.mongodb.net/?retryWrites=true&w=majority`
       )
       .then(() => {
         console.log("connected");
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const connection = require("./config/connection");
 const flightRouter = require("./Router/flight");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/flight", flightRouter);
 app.listen(8080, () => {
