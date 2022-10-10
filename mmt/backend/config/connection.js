9aecd11c6ddfffcbe7dc386049f7c48cfc4c03cc
patelam2211 <patelakash221199@gmail.com>
2022-10-10 13:31:38 +0530

    done

--- mmt/backend/config/connection.js
+++ mmt/backend/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 // const mongodb_url = process.env.MONGODB_URL;
-const mongodb_url = "mongodb+srv://manoranjansethi97:Yh0xVyKumb9CjL73@cluster0.icyzirl.mongodb.net/mmt?retryWrites=true&w=majority"
+ const mongodb_url = "mongodb+srv://manoranjansethi97:Yh0xVyKumb9CjL73@cluster0.icyzirl.mongodb.net/mmt?retryWrites=true&w=majority"
 const connection = async() =>{
 
    return new Promise((resolve,reject)=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/makemytrip").then(()=>{
+    mongoose.connect(mongodb_url).then(()=>{
         console.log('connected')
         resolve()
     }).catch((err)=>{
