ef932362715b945e3847b44da1d7a0438d615b6e
Akash Surve <surveakash01@gmail.com>
2022-10-10 17:47:29 +0530

    update data

---
+++ Model/Users.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: { type: String, require: true },
+  email: { type: String, require: true },
+  mobilenumber: { type: Number, require: true },
+  password: { type: String, require: true },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const connection = require("./config/connection");
 const flightRouter = require("./Router/flight");
+
+const authRouter = require("./Router/auth.routes.js");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/flight", flightRouter);
+app.use("/auth", authRouter);
 app.listen(port, () => {
   connection();
   console.log("start server");
